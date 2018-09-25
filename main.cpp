21aa774e2175651b21f6867090b1c947d44f1f4d
Leonardo Calasans <calasans.leonardo@gmail.com>
2018-09-25 13:51:29 -0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include "Pessoa.h"
+#include "Atividade.h"
+
+using namespace std;
+
+
+int main() {
+
+ //test Pessoa
+    //Pessoa *maria = new Pessoa("Maria",2,4);
+    //cout << "getNome test : " << maria->getNome() << endl;
+    //cout << "getValorPorHora test : " << maria -> getValorPorHora() << endl;
+    //cout << "getHorasDiarias test : " << maria -> getHorasDiarias() << endl;
+    //cout << "getCusto test : " << maria -> getCusto(2) << endl;
+   // maria->imprimir();
+//-------------------------------
+
+
+//test Atividade
+
+   /* 
+    Atividade *brita =  new Atividade("Brita", 10);
+    cout << "getNome test : " << brita -> getNome() << endl;
+    cout << "getHoras test : " << brita -> getHorasNecessarias() << endl;
+    cout << "getQuantPessoas test1 : " << brita -> getQuantidadeDePessoas() << endl;
+    
+    //brita.adicionar(pessoa);
+    
+    cout << "getQuantPessoas test2 : " << brita->getQuantidadeDePessoas() << endl;
+    cout << "getDuracao test : " << brita->getDuracao()<< endl;
+    cout << "getCusto test : " << brita->getCusto() << endl;
+    
+    brita->imprimir();
+    */
+    return 0;
+}
+
